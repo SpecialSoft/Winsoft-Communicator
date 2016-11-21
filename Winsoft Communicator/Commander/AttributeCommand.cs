700ce6feac1db9d088275866da9fe47b4b2fa788
Arnyminer Z <Arnyminer.Z@gmail.com>
2016-11-21 19:53:09 +0100

    Add project files.

---
+++ Winsoft Communicator/Commander/AttributeCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winsoft_Communicator.Commander
+{
+    public class AttributeCommand
+    {
+        private string command;
+        private string[] attributesNames;
+
+        public AttributeCommand(string command, string[] attributesNames)
+        {
+            this.command = command;
+            this.attributesNames = attributesNames;
+        }
+
+        public void Call(string[] attributes)
+        {
+            if (attributes.Length == attributesNames.Length)
+            {
+                RawCall(attributes);
+            }
+            else if (attributes.Length < attributesNames.Length ||
+                     attributes.Length > attributesNames.Length)
+                throw new IndexOutOfRangeException("You must specify " + attributesNames.Length + " attributes");
+        }
+
+        public virtual void RawCall(string[] attributes)
+        {
+            string attributesToShow = "";
+            foreach(string attribute in attributes)
+            {
+                if (attributesToShow == "")
+                    attributesToShow = attribute;
+                else
+                    attributesToShow += ", " + attribute;
+            }
+            Console.WriteLine("Called command " + command + ". With attributes ");
+        }
+    }
+}
---
+++ Winsoft Communicator/Commander/ClearCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winsoft_Communicator
+{
+    public class ClearCommand
+    {
+        private string command;
+
+        public ClearCommand(string command)
+        {
+            this.command = command;
+        }
+
+        public virtual void OnCall()
+        {
+            Console.WriteLine("Called command " + command);
+        }
+    }
+}
