201228cbcbed88abcf9069e31faf2bc6ce7cbc1d
Arnyminer Z <Arnyminer.Z@gmail.com>
2016-11-21 20:05:39 +0100

    - Added attribute command - Added clear command - Added Commanders class

---
+++ Winsoft Communicator/Commanders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Winsoft_Communicator.Commander;
+
+namespace Winsoft_Communicator
+{
+    public class Commanders
+    {
+        private static List<AttributeCommand> attributeCommands = new List<AttributeCommand>();
+        private static List<ClearCommand> clearCommands = new List<ClearCommand>();
+
+        public static void AddCommand(AttributeCommand command)
+        {
+            attributeCommands.Add(command);
+        }
+        public static void AddCommand(ClearCommand command)
+        {
+            clearCommands.Add(command);
+        }
+    }
+}
